017558c5315cf1ed83735141a0ff68223bcc8908
Cyn3ah <62955916+Cyn3ah@users.noreply.github.com>
2020-08-10 14:15:57 -0500

    forgot to update

---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Welcome (props) {
+    return (
+        <div className="Welcome">
+            <h1>
+            Welcome, {props.name}!
+            </h1>
+        </div>
+    );
+}
+
+export default Welcome;
