70a789a5e67c0b8db42e37b8b59ba3d187ffd0ec
Cyn3ah <62955916+Cyn3ah@users.noreply.github.com>
2020-08-12 01:09:27 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-//import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import './App.css'
 import Welcome from "./components/welcome/Welcome"
 import Clock from "./components/clock/Clock"
 import Contact from "./components/contact/Contact"
+import Navigation from "./components/navigation/Navigation"
 
-function App() {
-  return (
-    <div className="App">
-      <Route path="/clock" component={Clock} />
-      <Route path="/contact" component={Contact} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+      <Navigation />
+      
+
+        <Switch>
+          <Route 
+            exact
+            path="/"
+            render={(props) => <Welcome {...props}
+            name="Cynthia"/>}
+          />
+          <Route 
+            path="/welcome/:name"
+            render={(props) => <Welcome {...props}
+            name={props.match.params.name} />}
+          />
+
+          <Route path="/clock" component={Clock} />
+          <Route path="/contact" component={Contact} />
+
+          <Route path="/error" component={Error} />
+        </Switch>
+          
+      
+      </div>
+    );
+  }
 }
 
 export default App;
