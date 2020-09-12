2be125bef07ee84544163ad0bd65ab689c2285bb
Cyn3ah <theg1obe@hotmail.com>
2020-09-12 14:24:08 -0500

    Updates

---
+++ precache-manifest.54f0a80c73f0e7652b67a4da732c8884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33dce6cec913aa8d143150002acb03b9",
+    "url": "/welcome-clock/index.html"
+  },
+  {
+    "revision": "eb17d3d60e7646133f69",
+    "url": "/welcome-clock/static/css/main.d0a08e73.chunk.css"
+  },
+  {
+    "revision": "777aae7e323a684c5a46",
+    "url": "/welcome-clock/static/js/2.794f91e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/welcome-clock/static/js/2.794f91e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb17d3d60e7646133f69",
+    "url": "/welcome-clock/static/js/main.29aa71b9.chunk.js"
+  },
+  {
+    "revision": "fa7a824b9fff42ba06e3",
+    "url": "/welcome-clock/static/js/runtime-main.532f10ea.js"
+  }
+]);
